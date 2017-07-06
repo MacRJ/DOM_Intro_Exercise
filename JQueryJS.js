c4b4ef6fe870795bc29c32167c44fb60dad56d3b
MacRJ <rmacwj@gmail.com>
2017-07-06 15:23:45 -0700

    added a new JQuery file. Redid the porblems using JQuery

---
+++ JQueryJS.js
@@ -0,0 +1,31 @@
+// question 1
+$("#greeting").text("Hello World!")
+
+
+// question 2
+$("li").css('backgroundColor', 'yellow')
+
+
+// question 3
+$('#greeting').append("<img src='http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif'>")
+
+// question 4
+$("li").mousedown(function(){
+  $("li").removeClass("selected");
+    $(event.target).addClass("selected");
+})
+
+// question 5
+$("li").mousedown(function(){
+  $("img[alt = 'beer']").attr("src", "images/" + $(event.target).html() + ".jpeg")
+})
+
+// question 6
+$("#ghosting").hover(function(){
+  $(this).remove();
+}, function() {
+  $(this).add();
+}
+);
+
+// question 7
--- myJS.js
+++ myJS.js
@@ -1,3 +1,5 @@
+// question 1
+
 var hello = document.getElementById('greeting');
 hello.innerHTML = "Hello World!";
 
@@ -6,6 +8,7 @@ for(var i = 0; i < list.length; i++){
   list[i].style.backgroundColor = 'yellow'
 }
 
+// question 2
   var img = document.createElement("IMG");
   img.src = 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif';
   img.id = 'gif';
@@ -38,19 +41,23 @@ elems[k].addEventListener('mousedown', changeImage);
 }
 
 
-// Question 5
+// Question 6
 var quest5 = document.getElementById('ghosting')
 function disapear(e){
   quest5.hidden = true
 }
 quest5.addEventListener('mouseover', disapear)
 
-// Question 6
+// Question 7
 var quest6 = document.getElementById('resize')
 
-function double(e){
-  quest6.setAttribute("style", "width: 400px;")
+  function double(e){
+    quest6.setAttribute("style", "width: 400px;")
 }
-quest6.addEventListener('mousedown', double)
+quest6.addEventListener('mouseover', double)
+
+for(var i = 0; i < quest6.length; i++){
+  quest6.setAttribute("style", "width: 200px;")
+}
 
 // Question 7
